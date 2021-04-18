4215e82ae870ca51a4d6b83e51eb0d121f91ce9b
Gabriel Brettas <gabrielbrettas@gmail.com>
2021-04-18 17:58:08 -0300

    Added Sass Structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import logo from './logo.svg';
 import Offers from './Offers';
 import CreateOffer from './CreateOffer';
-import './App.css';
+import './css/main.min.css';
+
+import logo from './assets/logo-horizontal.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+        <img src={logo} alt="Logo LocalTerra" className="logo"/>
+      </header>
+      <section className="App-header">
         <Offers />
         <CreateOffer />
-      </header>
+      </section>
     </div>
   );
 }
 
 export default App;
+  
